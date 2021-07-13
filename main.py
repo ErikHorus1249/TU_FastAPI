26d30c52af001480999c42fb7fad03c8d1cbb034
ErikHorus1249 <tuananh1421999@gmail.com>
2021-07-13 17:11:10 +0700

    fast api

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 from fastapi import FastAPI
 import fastapi
+from pydantic import BaseModel
+from typing import Optional
 
 
 app = FastAPI()
 
-@app.get("/")
-def index():
-    return {"data":{
-        "name":"erik",
-        "age":"22"
-    }}
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    publish: Optional[bool]
+
+
+@app.get("/blog")
+def index(limit, published:bool = True):
+    if published:
+        return {"data":f'{limit} publish blogs from db'}
+    else:
+        return {"data":'have no blog published'}
 
 @app.get("/about")
 def about():
     return {'data':'about the page!'}
+
+@app.get("/blog/{id}")
+def show(id:int):
+    return {'blog':id}
+
+@app.post("/blog")
+def create_blog(blog:Blog ):
+    return {'data':f'blog was created {blog.body}'}
+
+
